cec416c37d2a69e2cd4fa6f8f619bc6f544e1890
marcobuzzoni26 <marco.buzzoni88@gmail.com>
2022-02-08 17:29:53 +0100

    HTTP Stream https://angular.io/start/start-data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,14 +9,17 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { CartComponent } from './cart/cart.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   imports: [
     BrowserModule,
+    HttpClientModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
       { path: 'products/:productId', component: ProductDetailsComponent },
+      { path: 'cart', component: CartComponent },
     ]),
   ],
   declarations: [
